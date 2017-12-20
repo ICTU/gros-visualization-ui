ace6cafe5adec7096d1e7402cda197de5766e6e2
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-12-20 23:29:55 +0100

    Navigation: Multiple instances, generalize terms
    - Configuration option to set a prefix for the navigation; other hashes
      are always ignored.
    - Replace 'project' with 'item' for generalization
    - Let the callback for current item change provide a return value to
      decide if the first item should be selected when the initial hash
      is not in the list, and only do so if the list has items.
    - Callback for setting item link properties such as text and attributes.
    - Update documentation and unit tests

--- tests/index.js
+++ tests/index.js
@@ -93,6 +93,71 @@ describe('Navigation', () => {
             done();
         });
     });
+
+    it('Works with multiple navigations', (done) => {
+        const { window, d3, navigation } = setup('<div id="projects"></div><div id="times"></div>', done);
+        const projectsNavigation = new navigation({
+            container: '#projects',
+            prefix: 'project_'
+        });
+        projectsNavigation.start(projectsList);
+
+        const timeNavigation = new navigation({
+            container: '#times',
+            prefix: 'time_'
+        });
+        timeNavigation.start(['month', 'week', 'day', 'hour']);
+
+        const projects = d3.selectAll('#projects ul li'),
+              times = d3.selectAll('#times ul li');
+        var activeProject = 1;
+        var activeTime = 1;
+        var next = () => {};
+        function updateNavigation(hash, project, time, chain) {
+            activeProject = project;
+            activeTime = time;
+            next = chain;
+            window.location.hash = hash;
+        }
+        function checkNavigation() {
+            const hash = window.location.hash;
+            assert.isTrue(projects.filter(`:nth-child(${activeProject})`).classed('is-active'), `Project selected for ${hash}: ${activeProject}`);
+            assert.isTrue(times.filter(`:nth-child(${activeTime})`).classed('is-active'), `Time selected for ${hash}: ${activeTime}`);
+            next();
+        }
+
+        window.addEventListener("hashchange", checkNavigation);
+        checkNavigation();
+        updateNavigation("#project_FOO", 3, 1, () => {
+            updateNavigation("#time_week", 3, 2, () => {
+                updateNavigation("#day", 3, 2, () => {
+                    done();
+                });
+            });
+        });
+    });
+
+    it('Honors callback actions', (done) => {
+        const { window, d3, navigation } = setup('<div id="navigation"></div>', done);
+        window.location.hash = "#something_else";
+        const projectsNavigation = new navigation({
+            setCurrentItem: (item, hasItem) => {
+                // Pretend we do something with the item, and since it does
+                // not exist in the navigation we do not select any item, even
+                // the first.
+                return true;
+            },
+            addElement: (element) => {
+                element.text(d => `Project ${d}`);
+            }
+        });
+        projectsNavigation.start(projectsList);
+        const items = d3.selectAll('#navigation ul li'),
+              first = items.filter(":nth-child(1)");
+        assert.isFalse(first.classed('is-active'), 'First element not selected');
+        assert.equal(first.select('a').text(), 'Project BAR');
+        done();
+    });
 });
 
 describe('Spinner', () => {
